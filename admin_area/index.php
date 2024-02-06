db0dfda0fb0a773a31e8e42a99ddd101748b33dc
Shyamsundar Dattatraya Dudhbhate <111455190+shyam777100@users.noreply.github.com>
2024-02-06 23:34:15 +0530

    connect.php

--- admin_area/index.php
+++ admin_area/index.php
@@ -6,6 +6,9 @@
     <title>Admin Dashboard</title>
     <!-- bootstrap css link-->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <!-- Font Awesome link-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <!-- css file link-->
     <link rel="stylesheet" href="../style.css">
     <style>
@@ -13,6 +16,10 @@
     width: 100px;
     height: 100px;
     object-fit: contain;
+    }
+    .footer{
+    position: absolute;
+    bottom: 0;
     }
     </style>
 </head>
@@ -35,23 +42,23 @@
 
 <!-- Second Child -->
 <div class="bg-light">
-    <h3 class="text-center p-2">Manage Details</h3>
+    <h3 class="text-center p-1">Manage Products and Orders</h3>
 </div>
 
 <!-- third Child -->
 <div class="row">
-    <div class="col-md-12 bg-secondary p-1 d-flex align-items-center">
+    <div class="col-md-12 bg-secondary p-3 d-flex align-items-center">
         <div class="px-5">
-            <a href="#"><img src="../images/floorpot.png" alt="" class="admin_image"></a>
+            <a href="#"><img src="../images/Gouria-shop (3).png" alt="" class="admin_image"></a>
             <p class="text-light text-center">Admin Name</p>
         </div>
-        <!-- buttom*10>a.nav-link.text-light.bg-info.my-1-->
+        <!-- button*10>a.nav-link.text-light.bg-info.my-1-->
         <div class="button text-center">
-        <button><a href="" class="nav-link text-light bg-info my-1">Insert Products</a></button>
+        <button class="my-3"><a href="" class="nav-link text-light bg-info my-1">Insert Products</a></button>
         <button><a href="" class="nav-link text-light bg-info my-1">View Products</a></button>
-        <button><a href="" class="nav-link text-light bg-info my-1">Insert Categories</a></button>
+        <button><a href="index.php?insert_categories" class="nav-link text-light bg-info my-1">Insert Categories</a></button>
         <button><a href="" class="nav-link text-light bg-info my-1">View Categories</a></button>
-        <button><a href="" class="nav-link text-light bg-info my-1">Insert Brands</a></button>
+        <button><a href="index.php?insert_brands" class="nav-link text-light bg-info my-1">Insert Brands</a></button>
         <button><a href="" class="nav-link text-light bg-info my-1">View Brands</a></button>
         <button><a href="" class="nav-link text-light bg-info my-1">All Orders</a></button>
         <button><a href="" class="nav-link text-light bg-info my-1">All Payments</a></button>
@@ -59,6 +66,21 @@
         <button><a href="" class="nav-link text-light bg-info my-1">Logout</a></button>
         </div>
     </div>
+</div>
+<!-- Forth Child-->
+<div class="container my-5">
+    <?php
+    if(isset($_GET['insert_categories']))
+        include('insert_categories.php');
+        if(isset($_GET['insert_brands']))
+        include('insert_brands.php');
+    ?>
+
+</div>
+
+<!-- Last Child-->
+<div class="bg-info p-3 text-center footer">
+    <p>All rights reserved @-Samdata infotech-2024</P>
 </div>
 </div>
 
