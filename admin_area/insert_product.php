84a7f6cb25c46e821bc748992f23ca96edfe11a0
Shyamsundar Dattatraya Dudhbhate <111455190+shyam777100@users.noreply.github.com>
2024-02-07 23:48:32 +0530

    insert_product_form

--- admin_area/insert_product.php
+++ admin_area/insert_product.php
@@ -51,6 +51,35 @@
                 <option value="">Cat3</option>
                </select>
             </div>
+            <!-- Image 1 -->
+               <div class="form-outline mb-4 w-50 m-auto">
+                <label for="product_image1" class="form-label">Product image1</label>
+                <input type="file" name="product_image1" 
+                id="product_image1" class="form-control" required="required">
+            </div>
+             <!-- Image 2 -->
+             <div class="form-outline mb-4 w-50 m-auto">
+                <label for="product_image2" class="form-label">Product image2</label>
+                <input type="file" name="product_image2" 
+                id="product_image2" class="form-control" required="required">
+            </div>
+             <!-- Image 3 -->
+             <div class="form-outline mb-4 w-50 m-auto">
+                <label for="product_image3" class="form-label">Product image3</label>
+                <input type="file" name="product_image3" 
+                id="product_image3" class="form-control" required="required">
+            </div>
+             <!-- price -->
+             <div class="form-outline mb-4 w-50 m-auto">
+                <label for="product_price" class="form-label">Product Price</label>
+                <input type="text" name="product_price" 
+                id="product_price" class="form-control" placeholder="Enter product price" autocomplete="off" required="required">
+            </div>
+             <!-- price -->
+             <div class="form-outline mb-4 w-50 m-auto">
+               <input type="submit" name="insert_product" class="btn btn-info">
+            </div>
+            
         </form>
 
 
